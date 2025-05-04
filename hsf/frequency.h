2b879843b4e68ec5d1aec24e6e815a444d87fbfe
Tom He <tomhe@Toms-MacBook-Pro-3.local>
2025-05-03 20:37:14 -0400

    fix insert

--- hsf/frequency.h
+++ hsf/frequency.h
@@ -45,13 +45,11 @@ public:
         it->second = frequencies_[level].insert(std::move(node));
 
         auto new_frequency = it->second->first;
-        if (level > 0 && new_frequency > frequencies_[level - 1].begin()->first) {
-            std::cout << "Moving key " << key << " with new frequency " << new_frequency << " from level " << level << " to level " << level - 1 << std::endl;
-            std::cout << "Level " << level - 1 << ": size " << frequencies_[level - 1].size() << ", max freq " << frequencies_[level - 1].rbegin()->first << ", min freq " << frequencies_[level - 1].begin()->first << std::endl;
-            it = move_iterator(it, level - 1, new_frequency);
-            compact_level(level - 1);
-            fill_level(level);
+        for (int i = level; i > 0 && new_frequency > frequencies_[i - 1].begin()->first; i--) {
+            it = move_iterator(it, i-1, new_frequency);
+            compact_level(i-1);
         }
+        fill_level(level);
 
         return it;
     }
@@ -119,18 +117,23 @@ private:
         if (level == 0) {
             assert(frequencies_.size() > 1 || level_size >= min_cap);
             return;
+        } else if (level == parent_type::levels() - 1 || level_size >= min_cap) {
+            return;
         }
 
         assert(!frequencies_[level - 1].empty());
-        for (int i = 0; i <= level; i++) {
-            std::cout << "Level " << i << ": size " << frequencies_[i].size() << ", max freq " << frequencies_[i].rbegin()->first << ", min freq " << frequencies_[i].begin()->first << std::endl;
-        }
-        std::cout << std::endl;
         assert(frequencies_[level - 1].begin()->first >= frequencies_[level].rbegin()->first);
         
         auto [min_freq, min_key] = *frequencies_[level - 1].begin();
         move_key(min_key, level - 1, level, min_freq);
         fill_level(level - 1);
+    }
+
+    void print_levels(size_type level) {
+        for (int i = 0; i <= level; i++) {
+            std::cout << "Level " << i << ": size " << frequencies_[i].size() << ", max freq " << frequencies_[i].rbegin()->first << ", min freq " << frequencies_[i].begin()->first << std::endl;
+        }
+        std::cout << std::endl;
     }
 };
 
